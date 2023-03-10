d9e856af4b4d39adfcd93f70194867f8290c4b71
Patrick Siqueira <psique.estuda@gmail.com>
2023-03-10 00:16:16 -0300

    Decisão do estilo de card e inicio da organizacao dos arquivos de estilização

--- resources/projeto_hyper_pong_react/src/Pages/Home.tsx
+++ resources/projeto_hyper_pong_react/src/Pages/Home.tsx
@@ -1,5 +1,4 @@
 import CardEvento, {Status} from "../components/CardEvento/CardEvento";
-import CardEvento_Modelo from "../components/CardEvento/CardEvento_Modelo";
 import Header from "../components/header/Header";
 
 export default function Home() {
@@ -9,6 +8,6 @@ export default function Home() {
         <CardEvento bg="#47D144" data="02/12/2022" titulo="Rachão Fevereiro"  status={Status[0]}/>
         <CardEvento bg="#F84141" data="03/05/2023" titulo="Copa Hyper Março" status={Status[1]}/>
         <CardEvento bg="#F0C838"  data="04/07/2022" titulo="Rachão Abril" status={Status[0]}/>
-        <CardEvento_Modelo bg="#F0C838"  data="04/07/2022" titulo="Rachão Abril" status={Status[0]}/>
+        <CardEvento bg="#F0C838"  data="04/07/2022" titulo="Rachão Abril" status={Status[0]}/>
     </>
 }
