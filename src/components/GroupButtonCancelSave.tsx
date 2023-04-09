c50946486686cdbe8ede0009fadb0df64706f17e
Patrick Siqueira <psique.estuda@gmail.com>
2023-04-09 12:04:04 -0300

    Funcionalidade do botão Cancelar nos envios de formulários

--- src/components/GroupButtonCancelSave.tsx
+++ src/components/GroupButtonCancelSave.tsx
@@ -1,8 +1,12 @@
 import {ButtonSave, ContainerButtons, ButtonCancel} from "./styles/GroupButtonCancelSave";
+import {useNavigate} from "react-router-dom";
 
 const GroupButtonCancelSave = ()=>{
+
+    const navigation = useNavigate();
+
     return <ContainerButtons>
-        <ButtonCancel type="button">Cancelar</ButtonCancel>
+        <ButtonCancel type="button" onClick={()=>navigation(-1)}>Cancelar</ButtonCancel>
         <ButtonSave type="submit" >Salvar</ButtonSave>
     </ContainerButtons>
 }
