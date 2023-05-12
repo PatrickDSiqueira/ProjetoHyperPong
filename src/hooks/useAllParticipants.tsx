18a3f59ba3ae56c92a4a58ad593fdd85cd14e131
Lucas de Oliveira <lucas.sangrecco@gmail.com>
2023-05-12 12:47:03 -0300

    Card 11 - Criar tela de Not Found

--- src/hooks/useAllParticipants.tsx
+++ src/hooks/useAllParticipants.tsx
@@ -1,9 +1,12 @@
 import {useEffect, useState} from "react";
 import {ParticipantType} from "../types/types";
 import {database, onValue, ref} from "../FirebaseService";
+import { useNavigate } from "react-router-dom";
 
 export function useAllParticipants(setVisibleLoading: React.Dispatch<React.SetStateAction<boolean>>, idEvent: string | undefined, idCategory: string | undefined, controlador ?:any) {
     const [participants, setParticipants] = useState<ParticipantType[]>([]);
+
+    const navigate = useNavigate()
 
     useEffect(() => {
             setVisibleLoading(true)
@@ -12,11 +15,17 @@ export function useAllParticipants(setVisibleLoading: React.Dispatch<React.SetSt
             const participantsRef = ref(database, `events/${idEvent}/categories/${idCategory}/participants`);
 
             onValue(participantsRef, (snapshot) => {
-                snapshot.forEach((elem) => {
-                    allParticipants.push(elem.val())
-                });
-                setParticipants(allParticipants);
-                setVisibleLoading(false);
+                
+                if (snapshot.exists()) {
+                    snapshot.forEach((elem) => {
+                        allParticipants.push(elem.val())
+                    });
+                    setParticipants(allParticipants);
+                    setVisibleLoading(false);
+                }else{
+                    setVisibleLoading(false)
+                    navigate('/notfound')
+                }
             });
         },
         [setVisibleLoading, controlador]);
